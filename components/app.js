aba7d8a2df00db89c47d74a8294e1b4fdd27bccc
jinho6225 <jinho6225@hotmail.com>
2020-02-06 16:49:59 -0800

    post new grades

--- components/app.js
+++ components/app.js
@@ -5,11 +5,15 @@ class App {
     this.gradeForm = gradeForm;
     this.handleGetGradesError = this.handleGetGradesError.bind(this);
     this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
+    this.createGrade = this.createGrade.bind(this);
+    this.handleCreateGradeError = this.handleCreateGradeError.bind(this);
+    this.handleCreateGradeSuccess = this.handleCreateGradeSuccess.bind(this);
   }
 
   handleGetGradesError(error) {
     console.log(error)
   }
+
   handleGetGradesSuccess(grades) {
     this.gradeTable.updateGrades(grades)
     var total = 0
@@ -18,6 +22,26 @@ class App {
     }
     var computedAvg = total / grades.length;
     this.pageHeader.updateAverage(computedAvg)
+  }
+
+  createGrade(name, course, grade) {
+    $.ajax({
+      url: "http://sgt.lfzprototypes.com/api/grades",
+      method:"POST",
+      headers:{"x-access-token":"dO5Ox6r1"},
+      data:{ "name": name,
+            "course": course,
+            "grade": grade},
+      success: this.handleCreateGradeSuccess,
+      error: this.handleCreateGradeError
+    })
+  }
+
+  handleCreateGradeError(error) {
+    console.log(error)
+  }
+  handleCreateGradeSuccess() {
+    this.getGrades()
   }
 
   getGrades() {
@@ -29,8 +53,10 @@ class App {
       error: this.handleGetGradesError
     })
   }
+
   start() {
     this.getGrades()
+    this.gradeForm.onSubmit(this.createGrade)
   }
 
 }
