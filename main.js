ba6c0715ea02fd5b39d5395e849d4f82f56c17b8
jinho6225 <jinho6225@hotmail.com>
2020-02-08 14:10:45 -0800

    update feature completed

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 var table = document.querySelector('.table-striped')
 var header = document.querySelector('header')
 var form = document.querySelector('form')
-var pTag = document.querySelector('.d-none')
 
 var gForm = new GradeForm(form)
 var pHeader = new PageHeader(header)
-var gTable = new GradeTable(table, pTag)
+var gTable = new GradeTable(table)
 var app = new App(gTable, pHeader, gForm)
 
 app.start()
