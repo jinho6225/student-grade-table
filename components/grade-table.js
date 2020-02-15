6750144a2728a65e9df6d73876f2f4c1b0dbf1d6
jinho6225 <jinho6225@hotmail.com>
2020-02-14 16:44:54 -0800

    update btn updated

--- components/grade-table.js
+++ components/grade-table.js
@@ -40,15 +40,16 @@ class GradeTable {
     td4.classList.add("d-flex", "justify-content-around")
 
     var i1 = document.createElement('i')
-    var i2 = document.createElement('i')
+    var i2Btn = document.createElement('button')
     var i3 = document.createElement('i')
 
     td4.append(i1)
-    td4.append(i2)
+    td4.append(i2Btn)
     td4.append(i3)
 
     i1.classList.add('fas', 'fa-edit')
-    i2.classList.add('d-none', 'fas', 'fa-check-square')
+    i2Btn.classList.add('d-none', "btn", "btn-warning", "btn-sm")
+    i2Btn.textContent = 'Update'
     i3.classList.add('fas', 'fa-trash-alt')
 
     tr.appendChild(td1)
@@ -61,31 +62,35 @@ class GradeTable {
         deleteGrade(data.id)
       }
     })
-    i1.addEventListener('click', function(e) {
-      var a = e.target.parentElement.parentElement.querySelectorAll('td')
-      a[3].querySelectorAll('i')[1].classList.remove('d-none')
-      a[3].querySelectorAll('i')[0].classList.add('d-none')
 
-      for (let i = 0; i < a.length-1; i++) {
+    i1.addEventListener('click', function(e) {
+      var td = e.target.parentElement.parentElement.querySelectorAll('td')
+      e.target.nextElementSibling.classList.remove("d-none")
+      td[3].querySelectorAll('i')[0].classList.add('d-none')
+      for (let i = 0; i < td.length-1; i++) {
         var input = document.createElement('input')
         input.classList.add('inputClass')
-        var value = a[i].textContent;
-        a[i].textContent = ""
+        var value = td[i].textContent;
+        td[i].textContent = ""
         input.setAttribute('type', 'text')
         input.setAttribute('size', '7')
         input.setAttribute('value', value)
-        a[i].prepend(input)
+        td[i].prepend(input)
       }
     })
 
-    i2.addEventListener('click', function() {
+    i2Btn.addEventListener('click', update)
+    function update (e) {
       var arr = Array.from(document.querySelectorAll('.inputClass'))
       var obj = {}
       for (let i = 0; i < arr.length; i++) {
         obj[i] = arr[i].value
       }
       updateGrade(data.id, obj[0], obj[1], obj[2])
-    })
+    }
+
     tbody.appendChild(tr)
+
   }
+
 }
