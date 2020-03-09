35e62890b8c2c7fe08c275b83c9d8889f165ea12
Ubuntu <ubuntu@ip-172-31-1-53.us-east-2.compute.internal>
2020-03-09 20:06:21 +0000

    db/index.js

--- db/index.js
+++ db/index.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 
 var db = mysql.createConnection({
   host: "localhost",
-  user: "",
-  password: "",
+  user: "root",
+  password: "j1065718",
   database: "sgt"
 });
 
