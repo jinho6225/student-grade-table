e907452f15c6315cf5a3942b77a64561c139e9ac
jinho6225 <jinho6225@hotmail.com>
2020-06-01 16:08:24 -0700

    added .env file

--- db/index.js
+++ db/index.js
@@ -1,16 +1,18 @@
-/* eslint-disable no-console */
+/*global require, module, process*/
 
+require('dotenv').config();
 var mysql = require('mysql');
 
 var db = mysql.createConnection({
   host: 'localhost',
-  user: '',
-  password: '',
-  database: 'sgt'
+  user: 'root',
+  password: process.env.password,
+  database: 'sgt',
 });
 
-db.connect(err => {
+db.connect((err) => {
   if (err) throw err;
+  /* eslint-disable no-console */
   console.log('Connected!');
 });
 
--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
+/*global require, __dirname, process*/
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const router = require('./router.js');
 const path = require('path');
-const port = 3300;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -13,7 +15,7 @@ app.use(bodyParser.json());
 app.use('/', router);
 app.use('/', express.static(path.join(__dirname, '../client/dist')));
 
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
   /* eslint-disable no-console */
-  console.log(`app listening on port ${port}!`);
+  console.log(`app listening on port ${process.env.PORT}!`);
 });
