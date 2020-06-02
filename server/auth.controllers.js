8973eaef15d3ab5e1228e4dce7c34ec281d809cf
jinho6225 <jinho6225@hotmail.com>
2020-06-02 11:51:51 -0700

    installed express-validator, added logic for signup email, pwd

--- server/auth.controllers.js
+++ server/auth.controllers.js
@@ -1,18 +1,34 @@
-/*global require, module, NOW*/
+/*global require, module*/
 /* eslint-disable no-console */
 
 const db = require('../db/index.js');
+const { validationResult } = require('express-validator');
 
 const auth_controllers = {
   register_local: (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
     const { email, pwd } = req.body;
-    const qry =
-      'insert into user (email, pwd, submission_date) values (?, ?, SYSDATE())';
-    db.query(qry, [email, pwd], (err, result) => {
+    const qry = 'SELECT * FROM user WHERE `email` = ?';
+    db.query(qry, [email], (err, rows) => {
       if (err) {
-        res.status(400).send(err);
+        console.log(err, 'err');
       } else {
-        res.status(201).json(result);
+        if (rows.length != 0) {
+          res.send('user already exists.');
+        } else {
+          const qry =
+            'insert into user (email, pwd, submission_date) values (?, ?, SYSDATE())';
+          db.query(qry, [email, pwd], (err, result) => {
+            if (err) {
+              res.status(400).send(err);
+            } else {
+              res.status(201).json(result);
+            }
+          });
+        }
       }
     });
   },
--- server/auth.router.js
+++ server/auth.router.js
@@ -3,11 +3,31 @@
 const express = require('express');
 const auth = express.Router();
 const auth_controllers = require('./auth.controllers.js');
+const { check } = require('express-validator');
+const db = require('../db/index.js');
 
-auth.route('/register/local').post(auth_controllers.register_local);
-auth
-  .route('/register/local/list')
-  .get(auth_controllers.get_registered_list_local);
+auth.post(
+  '/register/local',
+  [
+    check('email')
+      .exists()
+      .withMessage('Email should not be empty')
+      .isEmail()
+      .normalizeEmail(),
+    check('pwd')
+      .exists()
+      .withMessage('Password should not be empty')
+      .isLength({ min: 8 })
+      .withMessage('minimum eight characters')
+      .matches(
+        /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$.!%*#?&])[A-Za-z\d@$.!%*#?&]{8,}$/
+      )
+      .withMessage('at least one letter, one number and one special character'),
+  ],
+  auth_controllers.register_local
+);
+
+auth.get('/register/local/list', auth_controllers.get_registered_list_local);
 
 // auth.route('/login/local').post(auth_controllers.login_local);
 
