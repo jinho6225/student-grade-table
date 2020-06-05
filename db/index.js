82ba84a2c5d0fb6db42c2add19a4e44404fe9f32
jinho6225 <jinho6225@hotmail.com>
2020-06-05 07:48:09 -0700

    install mysql2 and connected with db using sequelize

--- db/index.js
+++ db/index.js
@@ -1,19 +1,29 @@
 /*global require, module, process*/
 
 require('dotenv').config();
-var mysql = require('mysql');
+// var mysql = require('mysql');
+const { Sequelize } = require('sequelize');
 
-var db = mysql.createConnection({
+// var db = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: process.env.password,
+//   database: 'sgt',
+// });
+
+const db = new Sequelize('sgt', 'root', process.env.password, {
   host: 'localhost',
-  user: 'root',
-  password: process.env.password,
-  database: 'sgt',
+  dialect: 'mysql',
 });
 
-db.connect((err) => {
-  if (err) throw err;
-  /* eslint-disable no-console */
-  console.log('Connected!');
-});
+// db.connect((err) => {
+//   if (err) throw err;
+//   /* eslint-disable no-console */
+//   console.log('Connected!');
+// });
+
+db.authenticate()
+  .then(() => console.log('connected with SEQUELIZE'))
+  .catch((err) => console.log('somethin wrong'));
 
 module.exports = db;
