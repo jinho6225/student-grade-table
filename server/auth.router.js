b49581cdb659245857b50e1c77845ed17c14524e
jinho6225 <jinho6225@hotmail.com>
2020-06-04 21:00:00 -0700

    signup feature completed

--- server/auth.router.js
+++ server/auth.router.js
@@ -5,8 +5,10 @@ const auth = express.Router();
 const authCtrl = require('./auth.controller.js');
 
 auth.post('/register/local', authCtrl.localRegister);
-auth.post('/login/local', authCtrl.localLogin);
+
 auth.get('/exists/email/:value', authCtrl.exists);
+
+auth.post('/login/local', authCtrl.localLogin);
 auth.post('/logout', authCtrl.logout);
 
 module.exports = auth;
